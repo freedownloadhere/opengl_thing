045053cc0d056d86ca98002b3f5e4c7f9a39d371
fdh <freedownloadhere13@gmail.com>
2024-11-11 22:04:25 +0200

    kamera

--- src/Main.cpp
+++ src/Main.cpp
@@ -71,6 +71,23 @@ glm::vec3 cubePositions[] = {
     glm::vec3(-1.3f,  1.0f, -1.5f)  
 };
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+static void processKeys(GLFWwindow* window) {
+	const float cameraSpeed = 0.05f;
+
+	if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+}
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -143,6 +160,7 @@ int main() {
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+		processKeys(window);
 
 		glClearColor(0.0f, 0.3f, 0.45f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -158,12 +176,10 @@ int main() {
 		
 		for (int i = 0; i < 10; i++) {
 			glm::mat4 model = glm::mat4(1.0f);
-			glm::mat4 view = glm::mat4(1.0f);
-			float rotation = (2.0f * (i % 2) - 1.0f) * i * glfwGetTime();
+			const float rotation = (2.0f * (i % 2) - 1.0f) * (i + 1) * glfwGetTime() * 0.3f;
 			model = glm::translate(model, cubePositions[i]);
 			model = glm::rotate(model, rotation, glm::vec3(0.5f, 1.0f, 0.0f));
-			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-			//view = glm::rotate(view, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+			glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 			ourShader.setMat4("model", model);
 			ourShader.setMat4("view", view);
 			glDrawArrays(GL_TRIANGLES, 0, 36);
